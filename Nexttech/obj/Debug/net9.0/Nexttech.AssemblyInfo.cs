d25cc11d83cabc377c57713e3834b40b33e7eb8f
hijo-coder <hijranmaleh@gmail.dk>
2025-05-05 19:07:12 +0200

    update

--- Nexttech/obj/Debug/net9.0/Nexttech.AssemblyInfo.cs
+++ Nexttech/obj/Debug/net9.0/Nexttech.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nexttech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5b3d593161456aabe8599aeb91dcc5b0787199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d697fd41504303767dc1090b6ded201168a0a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nexttech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nexttech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Nexttech/wwwroot/script.js
+++ Nexttech/wwwroot/script.js
@@ -1,3 +1,9 @@
 function sayHello() {
     document.getElementById("output").innerText = "Hello from JavaScript!";
 }
+document.querySelectorAll('.settings-link').forEach(link => {
+    link.addEventListener('click', function(event) {
+      console.log("You clicked on: " + link.textContent);
+      // Optional: Add confirmation or animation
+    });
+  });
